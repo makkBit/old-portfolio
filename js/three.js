b7bbbd5765de95ab0d437b5569e6fd3edfd591e6
Makk <makk.bit@outlook.com>
2017-01-12 21:18:34 +0530

    add links to projects

--- js/three.js
+++ js/three.js
@@ -2,7 +2,7 @@ $(function(){
         $(".element").typed({
             strings: ["Computer Science Student", "full stack web developer",
             		 "JavaScript Ninja", "M^300a^300h^300a^300k"],
-            typeSpeed: -10,
+            typeSpeed: -40,
             showCursor: false
         });
     });
