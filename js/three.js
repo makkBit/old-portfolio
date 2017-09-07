602dde98a6e5541599b7621d1abc0bd67c7fdbd4
Mahak Narayan <makk.bit@outlook.com>
2017-09-07 23:06:33 +0530

    minor update

--- js/three.js
+++ js/three.js
@@ -1,7 +1,7 @@
 $(function(){
         $(".element").typed({
             strings: ["Computer Science Student", "full stack web developer",
-            		 "JavaScript Ninja", "M^300a^300h^300a^300k"],
+            		 "JavaScript Ninja", "M^100a^100h^100a^100k ^100N^100a^100r^100a^100y^100a^100n"],
             typeSpeed: -40,
             showCursor: false
         });
